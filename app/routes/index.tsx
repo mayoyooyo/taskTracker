8470d95ab5cf556cdd4339346821e52a1c86b774
mayoyooyo <ittiwat.35131@gmail.com>
2022-04-18 22:12:08 +0700

    init project

---
+++ app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div>
+      <div>mayo first page</div>
+    </div>
+  )
+}
---
+++ app/routes/task.tsx
@@ -0,0 +1,19 @@
+export default function Task() {
+  return (
+    <div>
+      <div className='container'>
+        <header>
+          <div>h1</div>
+          <button>add</button>
+        </header>
+      </div>
+      <div className='task-section'>
+        <div className='task'>
+          <div>topic</div>
+          <div>description</div>
+          <div>time</div>
+        </div>
+      </div>
+    </div>
+  )
+}
